43a6e76a412d00d9bb48d71ac2492c335db5976b
clarsongit <clarson@berkeley.edu>
2022-11-06 13:06:03 -0800

    Retrieve and show current city temperature

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+//// Display Day & Time of Current Location
 let now = new Date();
 let day = now.getDay();
 
@@ -26,3 +27,16 @@ let dayTime = `${day} ${hours}:${minutes}`;
 
 let displayDay = document.querySelector("h2");
 displayDay.innerHTML = dayTime;
+
+//// API Integration
+function displayTemp(response) {
+  let currentTemp = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = `${currentTemp}°`;
+}
+
+let city = "San Diego";
+let apiKey = "4f1c03a70540f345ecd651bb7c938490";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
